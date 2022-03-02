e5ddbdeec5e8510aba0c71c8d80198cb79192050
Marlodor96 <33927210+Marlodor96@users.noreply.github.com>
2022-03-02 12:25:23 +0100

    Added Modified Version StreamUnlocker to unlock only 1080p 60fps
    Added Modified Version StreamUnlocker.js to unlock only 1080p 60fps

---
+++ StreamUnlocker.plugin.js
@@ -0,0 +1,169 @@
+/**
+ * @name StreamUnlocker
+ * @website https://github.com/Marlodor96/Discord/
+ * @source https://github.com/Marlodor96/Discord/releases/download/Discord/StreamUnlocker.plugin.js
+ * @updateUrl https://github.com/Marlodor96/Discord/releases/download/Discord/StreamUnlocker.plugin.js
+ */
+/*@cc_on
+@if (@_jscript)
+	
+	// Offer to self-install for clueless users that try to run this directly.
+	var shell = WScript.CreateObject("WScript.Shell");
+	var fs = new ActiveXObject("Scripting.FileSystemObject");
+	var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\BetterDiscord\plugins");
+	var pathSelf = WScript.ScriptFullName;
+	// Put the user at ease by addressing them in the first person
+	shell.Popup("It looks like you've mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
+	if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
+		shell.Popup("I'm in the correct folder already.", 0, "I'm already installed", 0x40);
+	} else if (!fs.FolderExists(pathPlugins)) {
+		shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
+	} else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
+		fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
+		// Show the user where to put plugins in the future
+		shell.Exec("explorer " + pathPlugins);
+		shell.Popup("I'm installed!", 0, "Successfully installed", 0x40);
+	}
+	WScript.Quit();
+
+@else@*/
+module.exports = (() => {
+    const config = {
+        "info": {
+            "name": "Stream Unlocker",
+            "authors": [{
+                "name": "Marlodor96IT",
+                "discord_id": "Unknown",
+                "github_username": "Marlodor96"
+            
+            }],
+            "version": "Latest",
+            "description": "Unlock sharing your screen at 60fps 1080P everywhere!",
+            "github": "https://github.com/Marlodor96/Discord/",
+            "github_raw": "https://github.com/Marlodor96/Discord/releases/download/Discord/StreamUnlocker.plugin.js"
+        },
+		"changelog": [
+			{
+				"title": "Streaming Unlocker Permanently",
+				"type": "added",
+				"items": [
+					"Unlocked Streaming in 1080p - 60fps."
+				]
+			}
+			
+		],
+        "main": "StreamUnlocker.plugin.js"
+    };
+
+    return !global.ZeresPluginLibrary ? class {
+        constructor() {
+            this._config = config;
+        }
+        getName() {
+            return config.info.name;
+        }
+        getAuthor() {
+            return config.info.authors.map(a => a.name).join(", ");
+        }
+        getDescription() {
+            return config.info.description;
+        }
+        getVersion() {
+            return config.info.version;
+        }
+		getChangelog() {
+			return config.changelog;
+		}
+        load() {
+            BdApi.showConfirmationModal("Library Missing", `The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`, {
+                confirmText: "Download Now",
+                cancelText: "Cancel",
+                onConfirm: () => {
+                    require("request").get("https://github.com/Marlodor96/Discord/releases/download/Discord/0PluginLibrary.plugin.js", async (error, response, body) => {
+                        if (error) return require("electron").shell.openExternal("https://github.com/Marlodor96/Discord/releases/download/Discord/0PluginLibrary.plugin.js");
+                        await new Promise(r => require("fs").writeFile(require("path").join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"), body, r));
+                    });
+                }
+            });
+        }
+        start() {}
+        stop() {}
+    } : (([Plugin, Api]) => {
+        const plugin = (Plugin, Api) => {
+            const {
+                Patcher,
+                DiscordModules,
+                DiscordAPI,
+                Settings,
+                Toasts,
+                PluginUtilities
+            } = Api;
+            return class StreamUnlocker extends Plugin {
+                defaultSettings = {
+                    "screenSharing": false,
+                };
+                settings = PluginUtilities.loadSettings(this.getName(), this.defaultSettings);
+                originalNitroStatus = 0;
+                screenShareFix;
+                getSettingsPanel() {
+                    return Settings.SettingPanel.build(_ => this.saveAndUpdate(), ...[
+                        new Settings.SettingGroup("Features").append(...[
+                            new Settings.Switch("High Quality Screensharing", "Enable or disable 1080p/source @ 60fps screensharing. This adapts to your current nitro status.", this.settings.screenSharing, value => this.settings.screenSharing = value)
+                        ]),
+                        
+						
+                    ])
+                }
+                
+                saveAndUpdate() {
+                    PluginUtilities.saveSettings(this.getName(), this.settings)
+                    if (!this.settings.screenSharing) {
+                        switch (this.originalNitroStatus) {
+                            case 1:
+                                BdApi.injectCSS("screenShare", `#app-mount > div.layerContainer-yqaFcK > div.layer-2KE1M9 > div > div > form > div:nth-child(2) > div > div > div.flex-1xMQg5.flex-1O1GKY.horizontal-1ae9ci.horizontal-2EEEnY.flex-1O1GKY.directionRow-3v3tfG.justifyStart-2NDFzi.alignStretch-DpGPf3.noWrap-3jynv6.modalContent-BM7Qeh > div:nth-child(1) > div > button:nth-child(4) {
+                                    display: none;
+                                  }`)
+                                this.screenShareFix = setInterval(()=>{
+                                    document.querySelector("#app-mount > div.layerContainer-yqaFcK > div.layer-2KE1M9 > div > div > form > div:nth-child(2) > div > div > div.flex-1xMQg5.flex-1O1GKY.horizontal-1ae9ci.horizontal-2EEEnY.flex-1O1GKY.directionRow-3v3tfG.justifyStart-2NDFzi.alignStretch-DpGPf3.noWrap-3jynv6.modalContent-BM7Qeh > div:nth-child(1) > div > button:nth-child(3)").click()
+                                    clearInterval(this.screenShareFix)
+                                }, 100)
+                                break;
+                            default: //if user doesn't have nitro?
+                                BdApi.injectCSS("screenShare", `#app-mount > div.layerContainer-yqaFcK > div.layer-2KE1M9 > div > div > form > div:nth-child(2) > div > div > div.flex-1xMQg5.flex-1O1GKY.horizontal-1ae9ci.horizontal-2EEEnY.flex-1O1GKY.directionRow-3v3tfG.justifyStart-2NDFzi.alignStretch-DpGPf3.noWrap-3jynv6.modalContent-BM7Qeh > div:nth-child(1) > div > button:nth-child(4) {
+                                    display: none;
+                                  }
+                                  #app-mount > div.layerContainer-yqaFcK > div.layer-2KE1M9 > div > div > form > div:nth-child(2) > div > div > div.flex-1xMQg5.flex-1O1GKY.horizontal-1ae9ci.horizontal-2EEEnY.flex-1O1GKY.directionRow-3v3tfG.justifyStart-2NDFzi.alignStretch-DpGPf3.noWrap-3jynv6.modalContent-BM7Qeh > div:nth-child(1) > div > button:nth-child(3) {
+                                    display: none;
+                                  }
+                                  #app-mount > div.layerContainer-yqaFcK > div.layer-2KE1M9 > div > div > form > div:nth-child(2) > div > div > div.flex-1xMQg5.flex-1O1GKY.horizontal-1ae9ci.horizontal-2EEEnY.flex-1O1GKY.directionRow-3v3tfG.justifyStart-2NDFzi.alignStretch-DpGPf3.noWrap-3jynv6.modalContent-BM7Qeh > div:nth-child(2) > div > button:nth-child(3) {
+                                    display: none;
+                                  }`)
+                                this.screenShareFix = setInterval(()=>{
+                                    document.querySelector("#app-mount > div.layerContainer-yqaFcK > div.layer-2KE1M9 > div > div > form > div:nth-child(2) > div > div > div.flex-1xMQg5.flex-1O1GKY.horizontal-1ae9ci.horizontal-2EEEnY.flex-1O1GKY.directionRow-3v3tfG.justifyStart-2NDFzi.alignStretch-DpGPf3.noWrap-3jynv6.modalContent-BM7Qeh > div:nth-child(1) > div > button:nth-child(2)").click()
+                                    document.querySelector("#app-mount > div.layerContainer-yqaFcK > div.layer-2KE1M9 > div > div > form > div:nth-child(2) > div > div > div.flex-1xMQg5.flex-1O1GKY.horizontal-1ae9ci.horizontal-2EEEnY.flex-1O1GKY.directionRow-3v3tfG.justifyStart-2NDFzi.alignStretch-DpGPf3.noWrap-3jynv6.modalContent-BM7Qeh > div:nth-child(2) > div > button:nth-child(2)").click()
+                                    clearInterval(this.screenShareFix)
+                                }, 100)
+                            break;
+                        }
+                    }
+
+                    if (this.settings.screenSharing) BdApi.clearCSS("screenShare")
+				}
+                onStart() {
+                    this.originalNitroStatus = DiscordAPI.currentUser.discordObject.premiumType;
+                    this.saveAndUpdate()
+                    DiscordAPI.currentUser.discordObject.premiumType = 2
+                }
+
+                onStop() {
+                    DiscordAPI.currentUser.discordObject.premiumType = this.originalNitroStatus;
+                    this.removeClientsidePfp()
+					this.removeClientsideBanner()
+                    Patcher.unpatchAll();
+                }
+            };
+        };
+        return plugin(Plugin, Api);
+    })(global.ZeresPluginLibrary.buildPlugin(config));
+})();
+/*@end@*/
